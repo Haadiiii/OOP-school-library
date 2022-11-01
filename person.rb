9b4520cc4cec7ab46639d2072f68a2e8367f5bff
Haadiiii <hammali99@gmail.com>
2022-11-01 19:33:13 +0500

    Decorate person class by changeable name , capitalization and trimming classes

--- person.rb
+++ person.rb
@@ -1,5 +1,11 @@
-class Person
+require './nameable'
+require './decorator'
+require './capitalizedecorator'
+require './trimmerdecorator'
+
+class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -13,9 +19,22 @@ class Person
     @parent_permission || is_of_age?
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+person1 = CapitalizeDecorator.new(person)
+
+puts person1.correct_name
+
+person2 = TrimmerDecorator.new(person1)
+puts person2.correct_name
