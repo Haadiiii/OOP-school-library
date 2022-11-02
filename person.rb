4bce922bc52602b912ec8b9aaea64edec3f7435c
Haadiiii <hammali99@gmail.com>
2022-11-02 14:19:28 +0500

    Fix linters and add Association between rental and person

--- person.rb
+++ person.rb
@@ -4,16 +4,22 @@ require './capitalizedecorator'
 require './trimmerdecorator'
 
 class Person < Nameable
+  attr_reader :id, :rental
+  attr_accessor :name, :age
+
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
   end
 
-  attr_reader :id
-  attr_accessor :name, :age
+  def add_rental(rental)
+    @rental << rental
+    rental.person = self
+  end
 
   def can_use_services?
     @parent_permission || is_of_age?
